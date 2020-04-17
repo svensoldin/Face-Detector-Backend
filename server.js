408aa54236a7c94267b854f97714d3df93599770
Sven Soldin <soldinsven@gmail.com>
2020-04-17 13:44:25 +0200

    fix ssl2

--- server.js
+++ server.js
@@ -7,8 +7,8 @@ const signin = require('./controllers/signin');
 const profile = require('./controllers/profile');
 const image = require('./controllers/image');
 
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = 0;
 const db = knex({
-  process.env.NODE_TLS_REJECT_UNAUTHORIZED = 0;
   client: 'pg',
   connection: {
     connectionString : process.env.DATABASE_URL,
